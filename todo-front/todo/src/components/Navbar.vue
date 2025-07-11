fd975874301b96af8973b908a3433d5c531ea290
Daniel Pereira Lima (DSA | Weega Technologies) <daniel.lima@weega.com.br>
2025-07-11 12:45:09 -0300

    feature: adiciona mais elementos gráficos

--- todo-front/todo/src/components/Navbar.vue
+++ todo-front/todo/src/components/Navbar.vue
@@ -1,14 +1,29 @@
 <script lang="ts">
+    import Drawer from 'primevue/drawer';
+    import Button from 'primevue/button';
+
     export default {
+        data() {
+            return {
+                visible: true
+            }
+        },
+        components: {
+            Drawer,
+            Button
+        },
         methods: {
             goToHome() {
                 this.$router.push('/');
+                this.visible = false;
             },
             goToSobre() {
                 this.$router.push('/sobre')
+                this.visible = false;
             }, 
             goToTodasTarefas() {
                 this.$router.push('/todasTarefas')
+                this.visible = false;
             }
         }
     }
@@ -16,25 +31,38 @@
 
 
 <template>
-    <ul class="navbarUl">
-        <li class="home" @click="goToHome">
-            <img src="../assets/home_icon.svg" alt="">Home
-        </li>
-        <li class="todasTarefas" @click="goToTodasTarefas">
-            <img src="../assets/tarefa_icon.svg" alt="">Tarefas
-        </li>
-        <li class="sobre" @click="goToSobre">
-            <img src="../assets/sobre_icon.svg" alt="">Sobre
-        </li>
-    </ul>
+    <div class="navBar">
+        <Button class="botaoMenu" @click="visible = true" label="Menu"/>
+        <Drawer v-model:visible="visible" header="Menu">
+            <ul class="navbarUl">
+                <li class="home" @click="goToHome">
+                    <img src="../assets/home_icon.svg" alt="">Home
+                </li>
+                <li class="todasTarefas" @click="goToTodasTarefas">
+                    <img src="../assets/tarefa_icon.svg" alt="">Tarefas
+                </li>
+                <li class="sobre" @click="goToSobre">
+                    <img src="../assets/sobre_icon.svg" alt="">Sobre
+                </li>
+            </ul>
+        </Drawer>
+    </div>
 </template>
 
 <style scoped>
+.botaoMenu {
+    background: #F53003;
+}
+
+.botaoMenu:hover {
+    --p-button-primary-hover-background: #ca2302;
+}
 .navbarUl {
     list-style: none;
     display: flex;
-    align-items: center;
-    justify-content: space-around;
+    flex-direction: column;
+    padding-left: 0;
+    gap: 20px;
 }
 
 .navbarUl li:hover {
